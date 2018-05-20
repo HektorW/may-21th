d9a9f11cc1e2f4cb90033d7025687df24b5bbbe5
Hektor Wallin <hektorw@gmail.com>
2018-05-20 18:11:21 +0200

    Created node script for reading from serial port (untested)

---
+++ read-serial/index.js
@@ -0,0 +1,56 @@
+require('dotenv').load()
+
+const { appendFile, writeFileSync, existsSync, mkdirSync } = require('fs')
+const { join } = require('path')
+const SerialPort = require('serialport')
+
+const portName = process.env.PORT_NAME
+const baudRate = process.env.BAUD_RATE || 57600
+const filename = process.env.FILENAME || 'output.txt'
+
+if (!portName) {
+  console.log('You must specify a port name to use for serial communication')
+  process.exit()
+}
+
+console.log('Setting up', {
+  portName,
+  baudRate,
+  filename
+})
+
+const folderName = 'output'
+const outputFolder = join(__dirname, folderName)
+const outputDestination = join(outputFolder, filename)
+
+if (!existsSync(outputFolder)) {
+  mkdirSync(outputFolder)
+  console.log(`Created output folder "${outputFolder}"`)
+}
+
+if (!existsSync(outputDestination)) {
+  writeFileSync(outputDestination)
+  console.log(`Created output file "${outputDestination}"`)
+}
+
+var port = new SerialPort(portName, { baudRate }, error => {
+  if (error) {
+    console.log('Failed to open port')
+    throw error
+  }
+
+  console.log('Opened port')
+})
+
+port.on('error', error => {
+  console.log('Error from serialport', error)
+})
+
+port.on('data', data => {
+  console.log(data)
+  appendFile(outputDestination, data, error => {
+    if (error) {
+      console.log('Failed to write data to file', error)
+    }
+  })
+})
---
+++ read-serial/readme.md
@@ -0,0 +1,12 @@
+# Setup
+Create file `.env` and copy content from `.env.template`
+
+# List serialports
+In root of project:
+´´´
+./node_modules/.bin/serialport-list
+´´´
+
+Enter correct serialport name in `.env` file.
+
+
